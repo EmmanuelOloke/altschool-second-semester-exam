9b3a7b669bc41052c5e407c0cdbcd0de57cbb636
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2022-11-04 14:52:23 +0100

    not found page design

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './not-found.css';
 
+import Void from '../../assets/void.svg';
+
 const NotFound = () => {
-  return <>This is the 404 page</>;
+  return (
+    <div className="notfound-container">
+      <div className="not-found-details">
+        <div className="not-found-title">Page Not Found</div>
+        <img className="not-found-image" src={Void} alt="Error 404" />
+        <Link to="/">
+          <button className="home-btn">Go Back Home</button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default NotFound;
