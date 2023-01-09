b300605cc399d98f5b052a01947a28ed7d18d975
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2023-01-09 17:23:03 +0100

    new feature to show repo languages

--- src/components/Repos/Repos.jsx
+++ src/components/Repos/Repos.jsx
@@ -1,24 +1,54 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import LoadingSpinner from '../LoadingSpinner/LoadingSpinner';
 
 const Repos = ({ repos, isLoading }) => {
+
+  // repos array contains all info about each repo
+  // map over the repos array to get a new repoState extracting only relevant details into new Objects
+  // use the new repoState, map over it and use that to return the main UI.
+
+  const [repoState, setRepoState] = useState([])
+
+  const getLangFromRepoURL = async (url) => {
+    const response = await fetch(url);
+    const langOBJ = await response.json();
+    return langOBJ;
+  }
+
+
+  useEffect(() => {
+    const getLanguages = async () => {
+      const relevantDetails = await Promise.all(repos.map(async repo => {
+        const languages = await getLangFromRepoURL(repo.languages_url)
+        return {
+          id: repo.id, languages, name: repo.name,
+        }
+    }))
+      setRepoState(relevantDetails);
+  }
+    getLanguages()
+  }, [repos])
+  
+  console.log(repoState);
+
+
   if (isLoading) {
     return <LoadingSpinner />;
   }
 
-  return repos.map((repo, index) => (
-    <div className="repo-card" key={repo.id}>
-      <div>{repo.name}</div>
-
-      <Link className="link" to={`/repos/${repo.name}`} key={index}>
+  return (repoState.map((repoState, index) => (
+    <div className="repo-card" key={repoState.id}>
+      <div>{repoState.name.toUpperCase()}</div>
+      <div style={{fontSize: '0.9rem'}}>{(Object.keys(repoState.languages).length > 0 ? 'Languages: ' + Object.keys(repoState.languages).join(', ') : 'No Languages')}</div>
+      <Link className="link" to={`/repos/${repoState.name}`} key={index}>
         <button href="#" className="btn">
           View More
         </button>
       </Link>
     </div>
-  ));
+  )));
 };
 
 export default Repos;
